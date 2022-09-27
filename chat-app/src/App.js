955da62094151c2618b4bf834dee09b23e79d1f9
hungngo04 <ngotienhung195@gmail.com>
2022-09-27 00:19:03 -0400

    send message function

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  //testing
+  console.log(input);
+  console.log(messages);
+
+  //send the messages
+  const sendMessage = (event) => {
+    //prevent refreshing after submit form
+    event.preventDefault();
+    //store the previous messages after a new message appear
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chat app</h1>
+
+      <form>
+        {/* Input field */}
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        {/* Button */}
+        <button type='submit' onClick={sendMessage}>Send message</button>
+        {/* Input */}
+      </form>
+      
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
+
     </div>
   );
 }
